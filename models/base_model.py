5c63f4fb3947f4e548598adb7e48e6a97c516d35
vasitha1 <sulemfelsi@gmail.com>
2024-11-12 09:26:09 -0800

    Updating init method

--- models/base_model.py
+++ models/base_model.py
@@ -45,7 +45,7 @@ from datetime import datetime
 class BaseModel:
     """Defines common attributes/methods for other classes."""
 
-    def __init__(self, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
         """
         Instantiates public instance attributes.
 
@@ -56,7 +56,7 @@ class BaseModel:
                                 is updated
         """
 
-        if kwargs is None 
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
