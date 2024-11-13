550760630976c7aa756a059c487378297625d2d5
vasitha1 <sulemfelsi@gmail.com>
2024-11-13 13:12:32 -0800

    Task 7: Creating a command interpreter, adding docstrings and completing the help command

--- console.py
+++ console.py
@@ -6,21 +6,50 @@ This module contains the entry point of the command interpreter
 
 Methods:
 - quit and EOF methods: to exit the program
-- Customised help method: 
+- Customised help method: To summarise the use of all commands
+- Default: To handle situations where an unknown command is given to the prompt
+
+Usage:
+This class is intended to serve as a command interprete
+
+Design Notes:
+- The cmd module isneeded for the creation of an interactive interactively
+- line.strip makes sure that the line is empty after spaces and tabs removed
+- HBNBCommand().cmdloop() makes sure that code runs infinitely untill it meetan
+EOF or quit command like the shell does.
 """
 import cmd
 
 class HBNBCommand(cmd.Cmd):
+    """ Defines the commands as methods """
     prompt = '(hbnb)'
 
     def do_quit(self, arg):
+        """to exit the program using "quit" command"""
         return True
 
     def do_EOF(self, arg):
+        """to exit the program using Cntrl+D """
         return self.do_quit(arg)
 
-    def defaultf(self, line):
-        if line.strip() == '':
-            pass
+    def emptyline(self):
+        """Called when an empty line is entered."""
+        pass
+
+    def default(self, line):
+        """Is called when the command is not recorgnised """
+        print("Command not found: {}".format(line))
+
+    def do_help(self, arg):
+        """ Doccuments all created methods """
+        if arg == "quit":
+            print("Quit command to exit the program\n")
+        elif arg == "EOF":
+            print("Ctrl + D command to exit the program\n")
         else:
-            print("Command not found: {}".format(line))
+            print("\nDocumented commands (type help <topic>):")
+            print("========================================")
+            print("EOF  help  quit\n")
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
