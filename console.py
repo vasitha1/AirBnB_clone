479f02a76f43fb8fe09c339a76c030798a1e898c
Amiir25 <amirsadik636@gmail.com>
2024-11-14 20:04:02 +0300

    Update console.py

--- console.py
+++ console.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 console.py
 
@@ -11,10 +12,12 @@ Methods:
   nothing on empty input. By default pressing `Enter` without a command
   repeates the last command.
 - Customised help method: To summarise the use of all commands
-- Default: To handle situations where an unknown command is given to the prompt
+- Default: To handle situations where an unknown command is given to the
+  prompt
 
 Usage:
-This class is intended to serve as a command interpreter for the AirBnB console.
+This class is intended to serve as a command interpreter for the AirBnB
+console.
 
 Design Notes:
 - Imports the `cmd` module which allows to write a line-oriented command
@@ -23,11 +26,15 @@ Design Notes:
   interacts with the interpreter.
 - The cmd module is needed for the creation of an interactive interactively
 - line.strip makes sure that the line is empty after spaces and tabs removed.
-- HBNBCommand().cmdloop() makes sure that code runs infinitely untill it meets an
-  EOF or quit command like the shell does.
+- HBNBCommand().cmdloop() makes sure that code runs infinitely untill it meets
+  an EOF or quit command like the shell does.
 """
 
 import cmd
+import shlex  	# To handle quoted strings properly
+from models import storage
+from datetime import datetime
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -36,12 +43,12 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """to exit the program using "quit" command"""
+        """Exits the program using `quit` command"""
 
         return True
 
     def do_EOF(self, arg):
-        """to exit the program using Cntrl+D """
+        """Exits the program using Ctrl+D """
 
         # Print new line when exiting via EOF
         print()
@@ -57,19 +64,19 @@ class HBNBCommand(cmd.Cmd):
         Is called when the command is not recorgnised
         print("Command not found: {}".format(line))
     """
+
     def do_create(self, arg):
-        """Creates a new instance of the basemode, prints it's id and saves"""
+        """Creates a new instance of the BaseModel, saves and prints it's id."""
 
         if not arg:
             print("** class name missing **")
             return
 
         try:
-            if arg == "Basemodel":
+            # If class name is valid, create an instance dynamically
+            if arg == "BaseModel":
                 instance = BaseModel()
-
-                # remember to save to file when base method is added
-                instacne.save()
+                instance.save()
                 print(instance.id)
 
             else:
@@ -81,13 +88,13 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, arg):
         """Prints string representation of an instance based on class name and id"""
 
-        args = arg.split()
+        args = shlex.split(arg)
 
         if not args:
             print("** class name missing **")
             return
 
-        if args[0] != "Basemodel":
+        if args[0] != "BaseModel":
             print("** class doesn't exist **")
             return
 
@@ -107,13 +114,13 @@ class HBNBCommand(cmd.Cmd):
     def do_destroy(self, arg):
         """Deletes an instance based on class name and ID"""
 
-        args = arg.split()
+        args = shlex.split(arg)
 
         if not args:
             print("** class name missing **")
             return
 
-        if args[0] != "Basemodel":
+        if args[0] != "BaseModel":
             print("** class doesn't exist **")
             return
 
@@ -129,15 +136,16 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** no instance found **")
 
-    def all(self, arg):
+    def do_all(self, arg):
         """
         Prints all string representations of all instances and
         optionaly filters by class name.
         """
 
-        args = arg.split()
+        args = shlex.split(arg)
         objects = storage.all()
 
+        # If no class name is provided, print all instances
         if not args:
             print([str(obj) for obj in objects.values()])
 
@@ -150,13 +158,13 @@ class HBNBCommand(cmd.Cmd):
 
             print(filtered_objs)
 
-    def update(self, arg):
+    def do_update(self, arg):
         """
         Updates an instance based on the class name and ID by adding or
         updating an instance.
         """
 
-        args = arg.split()
+        args = shlex.split(arg)
 
         if not args:
             print("** class name missing **")
@@ -189,6 +197,29 @@ class HBNBCommand(cmd.Cmd):
         attribute_name = args[2]
         value = args[3]
 
+        if hasattr(obj, attribute_name):
+            attr_type = type(getattr(obj, attribute_name))
+            try:
+                value = attr_type(value)
+
+            except ValueError:
+                print("** invalid value type **")
+                return
+
+        else:
+            try:
+                if value.isdigit():
+                    value = int(value)
+
+                elif '.' in value:
+                    value = float(value)
+
+            except ValueError:
+                value = str(value)
+
+        setattr(obj, attribute_name, value)
+
+        """
         # Cast value to the appropriate type based on the attribute type
         if isinstance(getattr(obj, attribute_name, None), str):
             setattr(obj, attribute_name, value)
@@ -211,12 +242,13 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** attribute type unsupported **")
             return
+        """
 
         # Update the `updated_at` timestamp and save
         obj.updated_at = datetime.now()
         obj.save()
 
-"""
+    """
     def do_help(self, arg):
          Doccuments all created methods 
         if arg == "quit":
@@ -227,9 +259,8 @@ class HBNBCommand(cmd.Cmd):
             print("\nDocumented commands (type help <topic>):")
             print("========================================")
             print("EOF  help  quit\n")
-"""
+    """
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
-*** Unknown syntax
