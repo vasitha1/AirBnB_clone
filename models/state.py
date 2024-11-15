801f4b87a0f8199330fb11d70e25fef77124b277
Amiir25 <amirsadik636@gmail.com>
2024-11-15 09:29:45 +0300

    Add state.py

---
+++ models/state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+state.py
+
+This module provides a `State` class whcich inherits the
+'BaseModel' class
+"""
+
+
+class State(BaseModel):
+    """
+    Defines a class that inherits the `BaseModel` class
+
+    Attribute:
+        name (string) - Name of the State
+    """
+
+    name = ""
