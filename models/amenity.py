3d1084e797c63a8f7f8dd002a7e1b8e42a4c02b8
Amiir25 <amirsadik636@gmail.com>
2024-11-15 09:30:31 +0300

    Add amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+amenity.py
+
+This module provides a `Amenity` class whcich inherits the
+'BaseModel' class
+"""
+
+
+class Amenity(BaseModel):
+    """
+    Defines a class that inherits the `BaseModel` class
+
+    Attribute:
+        name (string) - Name of the Amenity
+    """
+
+    name = ""
