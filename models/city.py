ed49a9c5ee706152d41446d936a7ffb08d3f5a7f
Amiir25 <amirsadik636@gmail.com>
2024-11-15 09:30:10 +0300

    Add city.py

---
+++ models/city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+city.py
+
+This module provides a `City` class whcich inherits the
+'BaseModel' class
+"""
+
+
+class City(BaseModel):
+    """
+    Defines a class that inherits the `BaseModel` class
+
+    Attributes:
+        state_id (string) - Will store the state ID
+        name (string) - Name of the city
+    """
+
+    state_id = ""
+    name = ""
